be3cb84174067d76415dfbdb50f75c11bb89ff9f
Folletto <folletto.id@gmail.com>
2019-11-17 21:37:02 +0000

    Added WPM and reading speed test link.

--- options/js/goo.options.js
+++ options/js/goo.options.js
@@ -22,7 +22,7 @@ export class Options {
         <h2>Options</h2>
         <card>
           <div class="card-row">
-            <label>Reading speed (wpm)</label>
+            <label>Reading speed (<a href="https://en.wikipedia.org/wiki/Words_per_minute">wpm</a>)</label>
             <div class="content">
               <input id="readingSpeed" type="number" value="${this.getReadingSpeed()}" />
             </div>
@@ -31,6 +31,15 @@ export class Options {
             <div class="content">
               <button class="card-button" ${goo.onClick(this.onClickSave.bind(this))}>Save options</button>
             </div>
+          </div>
+        </card>
+        <h2>Info</h2>
+        <card>
+          <div class="card-row">
+            <label>
+              The average reading speed is between 150 and 210.<br />
+              To measure your reading speed, you can use <a href="https://www.myreadspeed.com/calculate/">this website</a>.
+            </label>
           </div>
         </card>
       </div>
